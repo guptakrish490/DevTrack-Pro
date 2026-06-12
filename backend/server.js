340c5eb3efbbaf74818adb261d9a572aecebfdc8
guptakrish490 <guptakrish490@gmail.com>
2026-06-12 22:40:35 +0530

    Setup Express backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("Backend working")
+})
+
+
+const port=3000
+
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`)
+})
