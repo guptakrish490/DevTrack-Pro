ef1a9e944d28af4b48201931527f5d81d3143034
guptakrish490 <guptakrish490@gmail.com>
2026-06-13 13:25:48 +0530

    Add authentication middleware

---
+++ backend/middlewares/authMiddlewares.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+export const verifyUser=async(req,res,next)=>{
+    try{
+        const decoded=jwt.verify(req.cookies.token,process.env.JWT_SECRET)
+        const user=await User.findById(decoded.id).select("-password")
+
+        if (!user) {
+            return res.status(401).json({ message: "Unauthorized: User not found" });
+        }
+
+        req.user=user
+        next()
+    }
+    catch(err){
+        return res.status(401).json({ message: "Unauthorized: Invalid or expired token" });
+    }
+}
