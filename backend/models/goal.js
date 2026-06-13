c1dab8c106d10f8c3d4ce7f679980dacf371a021
guptakrish490 <guptakrish490@gmail.com>
2026-06-13 14:59:24 +0530

    Implement goal schema

---
+++ backend/models/goal.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const goalSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    title:{
+        type:String,
+        required:true,
+        maxlength:50
+    },
+    description:{
+        type:String,
+        required:true,
+        maxlength:100
+    },
+    progress:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    startDate:{
+        type:Date,
+        default:Date.now,
+    },
+    endDate:{
+        type:Date,
+        default:null,
+    },
+    isCompleted:{
+        type:Boolean,
+        default:false
+    }
+
+}, { timestamps: true })
+
+
+const Goal = mongoose.model('Goal', goalSchema)
+export default Goal
