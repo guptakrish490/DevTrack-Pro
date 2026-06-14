899cbaf6ce4c7b626972a15bf7a4626ab46ee18f
guptakrish490 <guptakrish490@gmail.com>
2026-06-14 15:30:40 +0530

    Implement task updation endpoint

--- backend/controllers/taskControllers.js
+++ backend/controllers/taskControllers.js
@@ -26,14 +26,35 @@ export const createTasks = async (req, res) => {
     }
 }
 
-export const readTasks= async(req,res)=>{
-    try{
-        const user=req.user
-        const tasks=await Task.find({user:user._id})
+export const readTasks = async (req, res) => {
+    try {
+        const user = req.user
+        const tasks = await Task.find({ user: user._id })
 
         res.status(200).json(tasks)
     }
-    catch(err){
-        res.status(500).json({error:err.message})
+    catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
+export const updateTasks = async (req, res) => {
+    try {
+        const { newTitle, newDescription, newRelatedProject, newPriority, newStatus, newStartDate, newCompletedAt, newDueDate } = req.body
+        const task = await Task.findByIdAndUpdate(req.params.id, {
+            title: newTitle,
+            description: newDescription,
+            relatedProject: newRelatedProject,
+            priority: newPriority,
+            status: newStatus,
+            startDate: newStartDate,
+            completedAt: newCompletedAt,
+            dueDate: newDueDate
+        }, { new: true })
+
+        res.status(200).json(task)
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message })
     }
 }
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { verifyUser } from '../middlewares/authMiddlewares.js'
-import { createTasks, readTasks } from '../controllers/taskControllers.js'
+import { createTasks, readTasks, updateTasks } from '../controllers/taskControllers.js'
 
 const router = express.Router()
 
 router.post("/", verifyUser, createTasks)
-router.get("/",verifyUser,readTasks)
-
+router.get("/", verifyUser, readTasks)
+router.put("/:id", verifyUser, updateTasks)
 export default router
