8ff7387b80f752dd759c74415d50e4bc215da2ca
guptakrish490 <guptakrish490@gmail.com>
2026-06-16 18:55:53 +0530

    Implement activity logging model

---
+++ backend/models/activity.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+
+const activitySchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    relatedGoal: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Goal',
+        required: false
+    },
+    relatedProject: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: false
+    },
+    relatedTask: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task',
+        required: false
+    },
+    type: {
+        type: String,
+        enum: [
+            "goal_created",
+            "goal_completed",
+            "project_created",
+            "project_completed",
+            "task_created",
+            "task_completed"
+        ],
+        required: true
+    }
+}, { timestamps: true })
+
+
+const Activity = mongoose.model('Activity', activitySchema)
+export default Activity
