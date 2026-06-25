98980bf0f7466d19ff464713c9d58b41df60b4fa
guptakrish490 <guptakrish490@gmail.com>
2026-06-25 17:55:09 +0530

    feat(app): integrate react-toastify for global toast notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,62 +9,79 @@ import Tasks from "./pages/Tasks/Tasks"
 import Activity from "./pages/Activity/Activity"
 import ProtectedRoute from "./components/auth/ProtectedRoute"
 import Layout from "./components/layout/Layout"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<AuthPage />} />
+    <>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
 
-      <Route path="/dashboard" element={
-        <ProtectedRoute>
-          <Layout>
-            <Dashboard />
-          </Layout>
-        </ProtectedRoute>
-      } />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Layout>
+              <Dashboard />
+            </Layout>
+          </ProtectedRoute>
+        } />
 
-      <Route path="/profile" element={
-        <ProtectedRoute>
-          <Layout>
-            <Profile />
-          </Layout>
-        </ProtectedRoute>
-      } />
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <Layout>
+              <Profile />
+            </Layout>
+          </ProtectedRoute>
+        } />
 
-      <Route path="/goals" element={
-        <ProtectedRoute>
-          <Layout>
-            <Goals />
-          </Layout>
-        </ProtectedRoute>
-      } />
+        <Route path="/goals" element={
+          <ProtectedRoute>
+            <Layout>
+              <Goals />
+            </Layout>
+          </ProtectedRoute>
+        } />
 
-      <Route path="/projects" element={
-        <ProtectedRoute>
-          <Layout>
-            <Projects />
-          </Layout>
-        </ProtectedRoute>
-      } />
+        <Route path="/projects" element={
+          <ProtectedRoute>
+            <Layout>
+              <Projects />
+            </Layout>
+          </ProtectedRoute>
+        } />
 
-      <Route path="/tasks" element={
-        <ProtectedRoute>
-          <Layout>
-            <Tasks />
-          </Layout>
-        </ProtectedRoute>
-      } />
+        <Route path="/tasks" element={
+          <ProtectedRoute>
+            <Layout>
+              <Tasks />
+            </Layout>
+          </ProtectedRoute>
+        } />
 
-      <Route path="/activity" element={
-        <ProtectedRoute>
-          <Layout>
-            <Activity />
-          </Layout>
-        </ProtectedRoute>
-      } />
+        <Route path="/activity" element={
+          <ProtectedRoute>
+            <Layout>
+              <Activity />
+            </Layout>
+          </ProtectedRoute>
+        } />
 
-    </Routes>
+      </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        pauseOnHover
+        draggable
+        theme="dark"
+        toastClassName="bg-[#18181f] text-[#dbe8f0] border border-white/20 rounded-xl shadow-lg"
+        bodyClassName="font-open-sans text-sm"
+        progressClassName="bg-violet-500"
+      />
+
+    </>
   )
 
 }
