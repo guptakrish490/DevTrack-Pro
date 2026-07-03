775bc591acfb3ecbf89bc5491e7cc59ee3a76648
guptakrish490 <guptakrish490@gmail.com>
2026-07-03 12:25:22 +0530

    improve button styling for small screens

--- frontend/src/components/ui/CreateButton.jsx
+++ frontend/src/components/ui/CreateButton.jsx
@@ -1,6 +1,6 @@
-const CreateButton = ({innerText, onClick}) => {
+const CreateButton = ({ innerText, onClick }) => {
   return (
-    <button onClick={onClick} className='cursor-pointer max-w-50 sm:h-9 h-8 text-center rounded-lg px-3 sm:px-5 py-1 text-sm sm:text-md sm:font-semibold bg-violet-600 text-white border-0'>{innerText}<i className="ri-add-line"></i></button>
+    <button onClick={onClick} className='cursor-pointer max-w-50 sm:h-9 h-6 text-center rounded-lg px-2 sm:px-5 py-1 text-[10px] sm:text-sm sm:font-semibold bg-violet-600 text-white border-0'>{innerText}<i className="ri-add-line"></i></button>
   )
 }
 
