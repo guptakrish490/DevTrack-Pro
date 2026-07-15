0a13045bd924fb75b5235333a6db34abcec28f3d
guptakrish490 <guptakrish490@gmail.com>
2026-07-15 17:33:39 +0530

    refactor(activities): remove getUniqueActivity and populate fields in getAllActivities

--- backend/controllers/activityControllers.js
+++ backend/controllers/activityControllers.js
@@ -15,23 +15,12 @@ export const getAllActivities = async (req, res) => {
         const sortOrder = sortBy === "oldest" ? 1 : -1;
 
         const activities = await Activity.find({ user: user._id })
-        res.status(200).json(activities)
-    }
-    catch (err) {
-        res.status(500).json({ error: err.message })
-    }
-}
+            .sort({createdAt:sortOrder})
+            .populate("relatedGoal")
+            .populate("relatedProject")
+            .populate("relatedTask")
 
-export const getUniqueActivity = async (req, res) => {
-    try {
-        const user = req.user
-
-        const activity = await Activity.findById(req.params.id);
-
-        if (!activity) {
-            return res.status(404).json({ error: "Activity not found" })
-        }
-        res.status(200).json(activity)
+        res.status(200).json(activities);
     }
     catch (err) {
         res.status(500).json({ error: err.message })
--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express'
 import { verifyUser } from '../middlewares/authMiddlewares.js'
-import { deleteAllActivities, getAllActivities, getUniqueActivity } from '../controllers/activityControllers.js'
+import { deleteAllActivities, getAllActivities } from '../controllers/activityControllers.js'
 
 const router = express.Router()
 
 router.get("/", verifyUser, getAllActivities)
-router.get("/:id", verifyUser, getUniqueActivity,)
 router.delete("/", verifyUser, deleteAllActivities)
 
 export default router
