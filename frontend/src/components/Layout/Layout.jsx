a13a1b48e2192059b46b5ea7b8938c7d752ed44a
guptakrish490 <guptakrish490@gmail.com>
2026-07-30 19:51:10 +0530

    chore(frontend): update site title, logo, and layout styles

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,6 +1,5 @@
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
-import bg from "../../assets/images/background.png"
 import { useState, useEffect } from "react"
 import axios from "axios"
 
@@ -8,7 +7,7 @@ import axios from "axios"
 const Layout = ({ children }) => {
 
   const [data, setData] = useState(null)
-  const [sidebarOpen, setSidebarOpen] = useState(()=>window.innerWidth>=640);
+  const [sidebarOpen, setSidebarOpen] = useState(() => window.innerWidth >= 640);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -23,20 +22,20 @@ const Layout = ({ children }) => {
 
   return (
     <div className="absolute left-0 top-0 h-full w-screen bg-[#09090e] flex">
-        
-        {/* overlay on small screen */}
-        {sidebarOpen && (<div className="fixed z-20 inset-0 bg-black/40 backdrop-blur-sm sm:hidden" onClick={() => setSidebarOpen(!sidebarOpen)} ></div>)}
-        
-        {/* sidebar */}
-        <Sidebar data={data} sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-        {/* navbar and main */}
-        <div className="flex-1 flex flex-col">
-          <Navbar data={data} sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          <main className="text-white overflow-y-auto scrollbar-custom p-4">
-            {children}
-          </main>
-        </div>
+      {/* overlay on small screen */}
+      {sidebarOpen && (<div className="fixed z-20 inset-0 bg-black/40 backdrop-blur-sm sm:hidden" onClick={() => setSidebarOpen(!sidebarOpen)} ></div>)}
+
+      {/* sidebar */}
+      <Sidebar data={data} sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      {/* navbar and main */}
+      <div className="flex-1 flex flex-col">
+        <Navbar data={data} sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <main className="text-white overflow-y-auto scrollbar-custom p-4">
+          {children}
+        </main>
+      </div>
 
     </div>
   )
